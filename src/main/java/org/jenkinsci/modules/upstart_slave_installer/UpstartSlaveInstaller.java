89fb30beec3bc33b3af5e384f49d301cb9084880
Jesse Glick <jglick@cloudbees.com>
2019-04-15 21:49:42 -0400

    Updates.

--- src/main/java/org/jenkinsci/modules/upstart_slave_installer/UpstartSlaveInstaller.java
+++ src/main/java/org/jenkinsci/modules/upstart_slave_installer/UpstartSlaveInstaller.java
@@ -1,9 +1,13 @@
 package org.jenkinsci.modules.upstart_slave_installer;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.Util;
+import hudson.model.TaskListener;
 import hudson.os.SU;
-import hudson.remoting.Callable;
 import hudson.util.StreamTaskListener;
+import java.io.File;
+import java.io.IOException;
+import jenkins.security.NotReallyRoleSensitiveCallable;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.jenkinsci.modules.slave_installer.AbstractUnixSlaveInstaller;
@@ -11,10 +15,6 @@ import org.jenkinsci.modules.slave_installer.InstallationException;
 import org.jenkinsci.modules.slave_installer.LaunchConfiguration;
 import org.jenkinsci.modules.slave_installer.Prompter;
 import org.jvnet.localizer.Localizable;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InterruptedIOException;
 
 /**
  * Performs slave installation via upstart.
@@ -33,6 +33,7 @@ public class UpstartSlaveInstaller extends AbstractUnixSlaveInstaller {
         return Messages._UpstartSlaveInstaller_ConfirmationText();
     }
 
+    @SuppressFBWarnings("DM_EXIT")
     @Override
     public void install(LaunchConfiguration params, Prompter prompter) throws InstallationException, IOException, InterruptedException {
         final File srcSlaveJar = params.getJarFile();
@@ -44,38 +45,56 @@ public class UpstartSlaveInstaller extends AbstractUnixSlaveInstaller {
         String rootUser = prompter.prompt("Specify the super user name to 'sudo' to","root");
         String rootPassword = prompter.promptPassword("Specify your password for sudo (or empty if you can sudo without password)");
 
-        SU.execute(listener,rootUser,rootPassword,new Callable<Void, IOException>() {
-            public Void call() throws IOException {
-                try {
-                    File slaveJar = new File(rootDir, "slave.jar");
-                    FileUtils.copyFile(srcSlaveJar, slaveJar);
-
-                    String conf = IOUtils.toString(getClass().getResourceAsStream("jenkins-slave.conf"));
-                    conf = conf
-                            .replace("{username}", userName)
-                            .replace("{jar}", slaveJar.getAbsolutePath())
-                            .replace("{args}", args);
-
-                    final String name = "jenkins-slave-" + instanceId;  // service name
-                    FileUtils.writeStringToFile(new File("/etc/init/"+name+".conf"), conf);
-
-                    Util.createSymlink(new File("/etc/init.d"), "/lib/init/upstart-job", name, listener);
-
-                    Process p = new ProcessBuilder("initctl","start",name).redirectErrorStream(true).start();
-                    p.getOutputStream().close();
-                    IOUtils.copy(p.getInputStream(),listener.getLogger());
-
-                    int r = p.waitFor();
-                    if (r!=0) // error, but too late to recover
-                        throw new IOException("Failed to launch  a service: " + r);
-
-                    return null;
-                } catch (InterruptedException e) {
-                    throw (InterruptedIOException)new InterruptedIOException().initCause(e);
-                }
-            }
-        });
+        SU.execute(listener,rootUser,rootPassword,new NotReallyRoleSensitiveCallableImpl(instanceId, rootDir, srcSlaveJar, userName, args, listener));
 
         System.exit(0);
+    }
+    private static class NotReallyRoleSensitiveCallableImpl extends NotReallyRoleSensitiveCallable<Void, IOException> {
+        private final String instanceId;
+        private final File rootDir;
+        private final File srcSlaveJar;
+        private final String userName;
+        private final String args;
+        private final TaskListener listener;
+        NotReallyRoleSensitiveCallableImpl(String instanceId, File rootDir, File srcSlaveJar, String userName, String args, TaskListener listener) {
+            this.instanceId = instanceId;
+            this.rootDir = rootDir;
+            this.srcSlaveJar = srcSlaveJar;
+            this.userName = userName;
+            this.args = args;
+            this.listener = listener;
+        }
+        @SuppressFBWarnings("DMI_HARDCODED_ABSOLUTE_FILENAME")
+        @Override
+        public Void call() throws IOException {
+            try {
+                File slaveJar = new File(rootDir, "slave.jar");
+                FileUtils.copyFile(srcSlaveJar, slaveJar);
+
+                String conf = IOUtils.toString(getClass().getResourceAsStream("jenkins-slave.conf"));
+                conf = conf
+                    .replace("{username}", userName)
+                    .replace("{jar}", slaveJar.getAbsolutePath())
+                    .replace("{args}", args);
+
+                final String name = "jenkins-slave-" + instanceId;  // service name
+                FileUtils.writeStringToFile(new File("/etc/init/" + name + ".conf"), conf);
+
+                Util.createSymlink(new File("/etc/init.d"), "/lib/init/upstart-job", name, listener);
+
+                Process p = new ProcessBuilder("initctl", "start", name).redirectErrorStream(true).start();
+                p.getOutputStream().close();
+                IOUtils.copy(p.getInputStream(), listener.getLogger());
+
+                int r = p.waitFor();
+                if (r != 0) { // error, but too late to recover
+                    throw new IOException("Failed to launch  a service: " + r);
+                }
+
+                return null;
+            } catch (InterruptedException e) {
+                throw new IOException(e);
+            }
+        }
     }
 }
